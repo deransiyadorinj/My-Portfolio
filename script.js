bcc18f42e30deedd3842b619696b5ae4d5fe6a1f
deransiyadorinj <libinliyandar@gmail.com>
2026-01-09 20:05:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Script loaded check
+console.log("Portfolio JavaScript is connected successfully");
+
+// Welcome message
+window.onload = function () {
+    console.log("Welcome to my portfolio website!");
+};
+
+// Simple alert on name click (optional)
+const header = document.querySelector("header h1");
+
+if (header) {
+    header.addEventListener("click", function () {
+        alert("Hello! Welcome to my portfolio 😊");
+    });
+}
